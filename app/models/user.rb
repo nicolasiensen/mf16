441622ba880a13c1d03a48ad8aa466820f052cb3
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-06-12 08:50:37 +0200

    Add User.subscribe_to_mailchimp_list and execute it after user is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_create { User.delay.subscribe_to_mailchimp_list(self.id) }
+
+  def self.subscribe_to_mailchimp_list(user_id)
+    user = User.find(user_id)
+
+    response = Gibbon::Request.lists(ENV["MAILCHIMP_LIST_ID"]).members.create(
+      body: {
+        email_address: user.email,
+        status: "subscribed"
+      }
+    )
+
+    user.update_attribute(:mailchimp_id, response["id"])
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".subscribe_to_mailchimp_list" do
+    it "should update the mailchimp_id" do
+      VCR.use_cassette("subscribe_to_mailchimp_list") do
+        user = User.create! email: "nicolas@mf16.com", password: 'topsecret', password_confirmation: 'topsecret'
+        User.subscribe_to_mailchimp_list(user.id)
+        expect(user.reload.mailchimp_id).to_not be(nil)
+      end
+    end
+  end
 end
