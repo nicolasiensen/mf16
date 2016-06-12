6609e00d48f06b54a37185e3a0258213bed6cefd
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-06-12 08:49:27 +0200

    Add webmock and vcr

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,15 @@ require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+require 'webmock/rspec'
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/vcr_cassettes"
+  config.hook_into :webmock
+end
+
+ActiveRecord::Base.logger.level = 1
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
