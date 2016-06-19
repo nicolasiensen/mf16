12a30d62b0701c8e7e61e1213513908e1e084be7
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-06-19 09:39:09 +0200

    Fix tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,14 @@ RSpec.describe User, type: :model do
   describe ".subscribe_to_mailchimp_list" do
     it "should update the mailchimp_id" do
       VCR.use_cassette("subscribe_to_mailchimp_list") do
-        user = User.create! email: "nicolas@mf16.com", password: 'topsecret', password_confirmation: 'topsecret'
+        user = User.create!(
+          first_name: "Nícolas",
+          last_name: "Iensen",
+          email: "nicolas@mf16.com",
+          password: 'topsecret',
+          password_confirmation: 'topsecret'
+        )
+
         User.subscribe_to_mailchimp_list(user.id)
         expect(user.reload.mailchimp_id).to_not be(nil)
       end
