35870391129dfa9fdb6835400d843f1f2fd14e6b
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-06-29 08:44:54 +0200

    Refactor flash messages into partials/components and change the body bg color

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,8 @@ module ApplicationHelper
   def card
     return content_tag(:div, class: "bg-white p3 rounded mb3") { yield }
   end
+
+  def flash_message options
+    render partial: "shared/flash", locals: options
+  end
 end
