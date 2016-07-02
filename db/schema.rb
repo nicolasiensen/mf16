09beb4158e9d94c1c9d10e05d52da314feea875b
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-07-02 14:34:49 +0200

    Add admin to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623053330) do
+ActiveRecord::Schema.define(version: 20160702121951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20160623053330) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "facebook_image"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
