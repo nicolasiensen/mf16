6af8cd2269eae117e1b682ddfe274954fe4108a9
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-07-14 01:47:40 +0200

    Update mailer

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,4 +1,6 @@
 class Notifier < ApplicationMailer
+  default from: "Democracia Nos Debates <contato@psolrj.com.br>"
+
   def welcome user
     @user = user
     mail(to: @user.email, subject: "Bem-vindo à campanha Marcelo Freixo 2016")
@@ -6,6 +8,6 @@ class Notifier < ApplicationMailer
 
   def debate_petition user
     @user = user
-    mail(to: @user.email, subject: "")
+    mail(to: @user.email, subject: "Valeu pelo apoio! Agora vamos botar mais pressão!")
   end
 end
