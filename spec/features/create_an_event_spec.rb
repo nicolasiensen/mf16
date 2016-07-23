9a4106f4aef190d6232af8d228493257c7fc7334
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-07-23 17:15:30 +0200

    Add feature tests

---
+++ spec/features/create_an_event_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature "CreateAnEvent", type: :feature do
+  context "when the form is valid" do
+    it "should create a new event" do
+    end
+
+    it "should redirect to the created event page" do
+    end
+  end
+
+  context "when the form is not valid" do
+    it "should not create a new event" do
+    end
+
+    it "should display form errors" do
+    end
+  end
+end
