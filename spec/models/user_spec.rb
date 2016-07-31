be609135f1efc994267dbb456c5b2058c9b31133
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-07-31 09:51:31 +0200

    Fix user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe ".subscribe_to_mailchimp_list" do
     it "should update the mailchimp_id" do
-      VCR.use_cassette("retrieve_and_subscribe_member_from_mailchimp_list") do
+      VCR.use_cassette("retrieve_and_subscribe_member_from_mailchimp_list", match_requests_on: [:method]) do
         user = create(:user)
         User.subscribe_to_mailchimp_list(user.id)
         expect(user.reload.mailchimp_id).to_not be(nil)
