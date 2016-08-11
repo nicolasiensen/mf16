6c42d4338a5437d63721ab1297aca24ab728054c
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-08-11 08:45:07 +0200

    Add slick.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require slick.js/slick.js
 //= require_tree .
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -22,3 +23,9 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-80795517-1', 'auto');
 ga('send', 'pageview');
+
+$(document).ready(function(){
+  $('.slick').slick({
+    arrows: false
+  });
+});
