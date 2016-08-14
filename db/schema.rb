0ebe8fcf6fea32e83a19750f0411528ddd264491
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-08-14 13:04:38 +0200

    Add description to group

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808193906) do
+ActiveRecord::Schema.define(version: 20160814105944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20160808193906) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.string   "theme"
+    t.text     "description"
   end
 
   create_table "settings", force: :cascade do |t|
