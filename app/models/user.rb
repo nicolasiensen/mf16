f43061dac953c4f245fb3c88197ec419b2e9c5ea
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-10-04 20:18:20 +0200

    Add favorite tasks integration with Mailchimp

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,7 +47,11 @@ class User < ActiveRecord::Base
       interests: {
         "#{ENV["MAILCHIMP_INTEREST_ID"]}" => true,
         "#{ENV["MAILCHIMP_SIGNED_DEBATE_PETITION_INTEREST_ID"]}" => self.has_signed_debate_petition
-      },
+      }.merge(
+        FAVORITE_TASKS.inject({}) do |h, t|
+          h.merge(t[:mailchimp_interest_id] => self.favorite_tasks.include?(t[:text]))
+        end
+      ),
       merge_fields: {
         FNAME: self.first_name,
         LNAME: self.last_name,
